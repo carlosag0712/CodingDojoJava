dd135ab360771321cfa5fbb31a282f9a83ccd151
carlos arosemena <ceag68@hotmail.com>
2017-10-31 18:31:28 -0400

    theorem

---
+++ src/codingDojo/PythagoreanTheorem.java
@@ -0,0 +1,13 @@
+package codingDojo;
+
+import java.lang.Math;
+
+/**
+ * Created by carlosarosemena on 2017-10-31.
+ */
+public class PythagoreanTheorem {
+    public double calculateHypotenuse(int legA, int legB){
+
+
+    }
+}
