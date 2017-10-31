b1624a897830ad21583e07b3572051389a7ac27f
carlos arosemena <ceag68@hotmail.com>
2017-10-31 18:57:45 -0400

    fizz

--- src/codingDojo/FizzBuzz/FizzBuzz.java
+++ src/codingDojo/FizzBuzz/FizzBuzz.java
@@ -7,16 +7,16 @@ public class FizzBuzz {
     public String fizzBuzz(int num){
         String message = "";
 
-        if(num%3==0){
+        if(num%3==0 && num%5==0){
             message = "Fizz";
         } else if(num%5==0){
             message = "Buzz";
-        }else if(num%3==0 && num%5==0){
+        }else if(num%3==0){
             message = "FizzBuzz";
         }else{
 
-            String nobuzz = String.format("%",num);
-            message = nobuzz ;
+
+            message = String.valueOf(num) ;
         }
 
         return message;
