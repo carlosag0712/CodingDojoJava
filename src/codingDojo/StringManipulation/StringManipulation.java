6f27d315b78a41d46e9e222bc71697d831a459e4
carlos arosemena <ceag68@hotmail.com>
2017-11-02 21:14:30 -0400

    String Manipulaiton

--- src/codingDojo/StringManipulation/StringManipulation.java
+++ src/codingDojo/StringManipulation/StringManipulation.java
@@ -6,10 +6,33 @@ package codingDojo.StringManipulation;
 public class StringManipulation {
     public String trimAndConcat(String str1, String str2){
         String trimmedconcated = "";
-
-        str1.trim();
-        str2.trim();
-        trimmedconcated = str1.concat(str2);
+        trimmedconcated = str1.trim()+str2.trim();
         return trimmedconcated;
+    }
+
+    public Integer getIndexOrNull(String word, char letter){
+        int index = word.indexOf(letter);
+
+        if(index == -1){
+            return null;
+        }else{
+            return  index;
+        }
+
+    }
+
+    public Integer getIndexOrNull(String str1, String str2){
+        int index = str1.indexOf(str2);
+
+        if(index ==-1){
+            return null;
+        }else{
+            return index;
+        }
+    }
+
+    public String concatSubString(String str1, int int1, int int2, String str2){
+        String  substring = str1.substring(int1,int2);
+        return substring + str2;
     }
 }
--- src/codingDojo/StringManipulation/StringManipulationTest.java
+++ src/codingDojo/StringManipulation/StringManipulationTest.java
@@ -9,9 +9,18 @@ public class StringManipulationTest {
         String trimmedconcated = smtc.trimAndConcat("  Hell0   ", "   Carlos   ");
         System.out.println(trimmedconcated);
 
+        StringManipulation gidx = new StringManipulation();
+        Integer index = gidx.getIndexOrNull("Carlos", 'x');
+        System.out.println(index);
+
+        StringManipulation gidx2 = new StringManipulation();
+        Integer index2 = gidx2.getIndexOrNull("Carlos", "losx");
+        System.out.println(index2);
+
+        StringManipulation consub = new StringManipulation();
+        String newword = consub.concatSubString("Carlos",0,1,"Arosemena");
+        System.out.println(newword);
 
     }
-
-
 
 }
