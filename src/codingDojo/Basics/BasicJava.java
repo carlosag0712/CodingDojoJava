8d63fea3663ad561c43be9e296a61925132a86e4
carlos arosemena <ceag68@hotmail.com>
2017-11-04 07:37:40 -0400

    javabasics

---
+++ src/codingDojo/Basics/BasicJava.java
@@ -0,0 +1,22 @@
+package codingDojo.Basics;
+
+/**
+ * Created by carlosarosemena on 2017-11-02.
+ */
+
+
+public class BasicJava {
+
+    public Integer print1to255(){
+
+        int [] print1To255;
+
+
+        for(int i =0; i<=255;i++ ){
+            print1To255 = [i];
+        }
+
+    }
+
+
+}
