0a92ac6f3188be915d0b8d6d271f576a58a4335e
carlos arosemena <ceag68@hotmail.com>
2017-11-04 11:13:42 -0400

    javabasics

--- src/codingDojo/Basics/BasicJava.java
+++ src/codingDojo/Basics/BasicJava.java
@@ -1,5 +1,7 @@
 package codingDojo.Basics;
 
+import java.util.ArrayList;
+
 /**
  * Created by carlosarosemena on 2017-11-02.
  */
@@ -7,16 +9,148 @@ package codingDojo.Basics;
 
 public class BasicJava {
 
-    public Integer print1to255(){
+    public void print1to255(){
 
-        int [] print1To255;
-
-
-        for(int i =0; i<=255;i++ ){
-            print1To255 = [i];
+        for(int i =1; i<=255;i++ ){
+            System.out.println(i);
         }
 
     }
+
+    public void print1To255Odds(){
+        for(int i=1; i<=255;i++){
+            if(i%2!=0){
+                System.out.println(i);
+            }
+        }
+    }
+
+    public void printSum(){
+        int sum =0;
+        for(int i=0; i<=255;i++){
+              sum += i;
+            System.out.println("New number: "+i+" Sum: "+sum);
+        }
+    }
+
+    public void iterateArray(int[] array){
+        for(int i =0;i< array.length;i++){
+            System.out.println(array[i]);
+
+        }
+    }
+
+    public void findMax(int[] array){
+        int max  = array[0];
+
+        for(int i = 1; i < array.length; i++){
+            if(array[i]>max){
+                max = array[i];
+            }
+        }
+        System.out.println(max);
+    }
+
+    public void getAverage(int[] array){
+        int sum = 0;
+        int lengtharr = array.length;
+
+        for(int i = 0; i<lengtharr;i++){
+            sum += array[i];
+
+        }
+
+        System.out.println(sum/lengtharr);
+    }
+
+    public void arrayOfOdds(){
+        ArrayList<Integer> arrayOdds = new ArrayList<>();
+
+        for (int i =1; i<=255; i++){
+            if(i%2 != 0){
+                arrayOdds.add(i);
+            }
+        }
+        System.out.println(arrayOdds);
+    }
+
+    public void greaterThanY(int y, int[]array){
+        int greaterThanY = 0;
+
+        for (int i=0; i<array.length;i++){
+            if(array[i]>y){
+                greaterThanY += 1;
+            }
+        }
+
+        System.out.println(greaterThanY+" numbers are greater than "+y);
+
+    }
+
+    public void squareTheValues(int[]array){
+        for(int i =1; i<array.length;i++){
+            array[i] *= array[i];
+        }
+
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+
+        }
+    }
+
+    public void eliminateNegNum(int[]array){
+        for(int i=0; i<array.length;i++){
+            if(array[i]<0){
+                array[i]=0;
+            }
+        }
+
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+
+        }
+    }
+
+    public void MinMaxAvg(int[]arr){
+        int max = arr[0];
+        int min = arr[0];
+        int sum = 0;
+
+        for(int i=0; i<arr.length;i++){
+            sum += arr[i];
+            if(arr[i]>max){
+                max = arr[i];
+            }
+            if(arr[i]<min){
+                min = arr[i];
+            }
+        }
+
+        int avg = sum / arr.length;
+
+        int [] minmaxavg = {max,min,avg};
+
+        for(int i=0;i<minmaxavg.length;i++){
+            System.out.println(minmaxavg[i]);
+        }
+    }
+
+    public void arrayShift(int[] arr){
+
+        for(int i =0;i<arr.length-1;i++){
+            arr[i]=arr[i+1];
+        }
+
+        arr[arr.length-1] = 0;
+
+        for(int i =0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+
+
+    }
+
+
 
 
 }
---
+++ src/codingDojo/Basics/BasicJavaTest.java
@@ -0,0 +1,23 @@
+package codingDojo.Basics;
+
+public class BasicJavaTest {
+    public static void main(String[] args) {
+        BasicJava bj = new BasicJava();
+//        bj.print1to255();
+//        bj.print1To255Odds();
+//        bj.printSum();
+        int [] array = {1,3,5,7,9,13};
+//        bj.iterateArray(array);
+//        bj.findMax(array);
+//        int [] array2 = {2,10,3};
+//        bj.getAverage(array2);
+//        bj.arrayOfOdds();
+//        bj.greaterThanY(3, array);
+        int [] array3 = {1,5,10,-2};
+//        bj.squareTheValues(array3);
+//        bj.eliminateNegNum(array3);
+//        bj.MinMaxAvg(array3);
+        bj.arrayShift(array3);
+
+    }
+}
