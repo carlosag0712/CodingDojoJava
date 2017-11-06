2e5f303b6cd2e3dbea53720e5e38f8ee1cd617f2
carlos arosemena <ceag68@hotmail.com>
2017-11-06 17:21:37 -0500

    JavaOOP

--- src/codingDojo/JAVA_OOP/Project_Class/Project_Class.java
+++ src/codingDojo/JAVA_OOP/Project_Class/Project_Class.java
@@ -3,13 +3,16 @@ package codingDojo.JAVA_OOP.Project_Class;
 public class Project_Class {
     private String name;
     private String description;
+    private double initialCost;
 
     public Project_Class(){
+
 
     }
 
     public Project_Class(String name) {
         this.name = name;
+
     }
 
     public Project_Class(String name, String description) {
@@ -17,6 +20,11 @@ public class Project_Class {
         this.description = description;
     }
 
+    public Project_Class(String name, String description, double initialCost) {
+        this.name = name;
+        this.description = description;
+        this.initialCost = initialCost;
+    }
 
     public String getName() {
         return name;
@@ -34,5 +42,15 @@ public class Project_Class {
         this.description = description;
     }
 
+    public String elevatorPitch(){
+        return "Project Name: "+this.name +"\n"+ "Project Description: "+this.description +"\n"+"Project Cost: "+this.initialCost;
+    }
 
+    public double getInitialCost() {
+        return initialCost;
+    }
+
+    public void setInitialCost(double initialCost) {
+        this.initialCost = initialCost;
+    }
 }
